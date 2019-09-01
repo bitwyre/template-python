e7bffb5fec9d017ea9beac73b1659f77df6250d6
Yefta Sutanto <yeftasutanto@gmail.com>
2019-09-01 22:38:09 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Repo/Module Name Here
+
+This is the template for Python Repository.
+
+Copyright (C) 2019 Bitwyre Technologies LLC
+
+## Running in Development Mode
+
+```sh
+$ run.sh --dev
+```
---
+++ setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, find_packages
+from os import path
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='template_python',
+    version='0.0.1',
+    description='Bitwyre Python Repo Template',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/bitwyre/template-python',
+    author='Bitwyre Technologies LLC',
+    author_email='dev@bitwyre.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: GNU GPLv3 License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    keywords='financial exchange cryptocurrency',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
+    python_requires='!=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4',
+    install_requires=[],  # Optional
+    extras_require={
+        'dev': ['check-manifest', 'pep8', 'mypy'],
+        'test': ['coverage', 'pytest'],
+    },
+    entry_points={
+        'console_scripts': [
+            'public_api=template_python:cli',
+        ],
+    },
+    project_urls={
+        'Bug Reports': 'https://github.com/bitwyre/template-python/issues',
+        'Funding': 'https://bitwyre.com',
+        'Say Thanks!': 'http://bitwyre.gives',
+        'Source': 'https://github.com/bitwyre/template-python',
+    },
+)
