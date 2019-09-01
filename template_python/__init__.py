f5c124c37c32410149951255b7b0d4a5a59ae163
Yefta Sutanto <yeftasutanto@gmail.com>
2019-09-01 22:49:19 +1000

    Remove Travis, add GH Workflow

--- template_python/__init__.py
+++ template_python/__init__.py
@@ -1,4 +1,21 @@
+import argparse
+import sys
 
-def main():
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Python Template")
+    args = parser.parse_args()
+    return args
+
+
+def main(args):
+    print(args)
+
+
+def cli():
     """Entry point for the application script"""
-    print("Call your main application code here")
+    args = parse_args()
+    try:
+        main(args)
+    except KeyboardInterrupt:
+        sys.exit(0)
